4eb5fd4c6ad6b1c13710d726b74b05df77dbf975
qiannn <409339559@qq.com>
2016-05-19 16:16:24 +0800

    dynamic programming

--- Scramble String/Scramble String/main.cpp
+++ Scramble String/Scramble String/main.cpp
@@ -8,11 +8,13 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
+    /*
     bool isScramble(string s1, string s2) {
         if (s1 == s2)
             return true;
@@ -38,13 +40,50 @@ public:
         }
         return false;
     }
+    */
+    bool isScramble(string s1, string s2) {
+        int len = s1.length();
+        bool res[len][len][len + 1];
+
+        for (int z = 0; z < len + 1; z++) {
+            for (int x = 0; x < len; x++) {
+                for (int y = 0; y < len; y++) {
+                    res[x][y][z] = false;
+                }
+            }
+        }
+        
+        for (int x = 0; x < len; x++) {
+            for (int y = 0; y < len; y++) {
+                res[x][y][1] = s1[x] == s2[y];
+            }
+        }
+        
+        //x:s1 index, y:s2 index, z:substring length
+        for (int z = 1; z < len + 1; z++) {
+            for (int x = 0; x + z < len + 1; x++) {
+                for (int y = 0; y + z < len + 1; y++) {
+                    for (int i = 1; i < z; i++) {
+                        res[x][y][z] =
+                        (res[x][y][i] && res[x + i][y + i][z - i])||
+                        (res[x][y + z - i][i] && res[x + i][y][z - i]);
+                        if (res[x][y][z])
+                            break;
+                    }
+                }
+            }
+        }
+        return res[0][0][len];
+    }
 };
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     Solution s;
-    if (s.isScramble("abcdefghijklmnopq", "efghijklmnopqcadb"))
+    string s1 = "xstjzkfpkggnhjzkpfjoguxvkbuopi", s2 = "xbouipkvxugojfpkzjhnggkpfkzjts";
+    //string s1 = "great", s2 = "rgtae";
+    if (s.isScramble(s1, s2))
         cout << "yes" << endl;
     else
         cout << "no" << endl;
