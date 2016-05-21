71b2de7e3dbdfea9d86969e4fd0c5a76b46c1123
qiannn <409339559@qq.com>
2016-05-21 16:17:48 +0800

    add Longest Substring Without Repeating Characters

---
+++ Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters/main.cpp
@@ -0,0 +1,65 @@
+//
+//  main.cpp
+//  Longest Substring Without Repeating Characters
+//
+//  Created by apple on 16/5/21.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.empty())
+            return 0;
+        int maxlen = 1, start = 0;
+        unordered_map<char, int> char_map;
+        char_map[s[start]] = start;
+        for (int p = 1; p < s.length(); p++) {
+            char cur_char = s[p];
+            auto repeatchar_iter = char_map.find(cur_char);
+            if (repeatchar_iter != char_map.end()) {
+                int pos = char_map[cur_char];
+                while(start <= pos) {
+                    char_map.erase(s[start++]);
+                }
+            }
+            char_map[cur_char] = p;
+            maxlen = max(p - start + 1, maxlen);
+        }
+        
+        return maxlen;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    Solution s;
+    //3
+    string s1("abcabcbb");
+    //1
+    string s2("bbbbb");
+    //3
+    string s3("pwwkew");
+    //13
+    string s4("tdknvjqvwrsvehvkheyooqegdkipsirnnbakmsfijyeoh");
+    
+    cout << s.lengthOfLongestSubstring(s1) << endl;
+    cout << s.lengthOfLongestSubstring(s2) << endl;
+    
+     cout << s.lengthOfLongestSubstring(s3) << endl;
+     cout << s.lengthOfLongestSubstring(s4) << endl;
+     //11
+     cout << s.lengthOfLongestSubstring("vaewogmglhxqbyqkdlaxuqyztrrksuenwkmkryrefqvions") << endl;
+     //
+     cout << s.lengthOfLongestSubstring("abc") << endl;
+    
+    return 0;
+}
