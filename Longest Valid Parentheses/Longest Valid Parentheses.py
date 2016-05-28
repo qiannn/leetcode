2da669c53781f7f54c7e76067f13ab1b6ff31155
qiannn <409339559@qq.com>
2016-05-28 15:46:53 +0800

    add stack solution

--- Longest Valid Parentheses/Longest Valid Parentheses.py
+++ Longest Valid Parentheses/Longest Valid Parentheses.py
@@ -1,9 +1,31 @@
 class Solution(object):
+
 	def longestValidParentheses(self, s):
 		"""
 		:type s: str
 		:rtype: int
 		"""
+		stack = []
+		maxlen = 0
+		for i in xrange(len(s)):
+			if s[i] == '(':
+				stack.append(i)
+			else:
+				if stack and s[stack[-1]] == '(':
+					stack.pop()
+					length = 0
+					if stack:
+						length = i - stack[-1]
+					else:
+						length = i + 1
+					maxlen = max(maxlen, length)
+				else:
+					stack.append(i) 
+
+		return maxlen
+
+	"""
+	def longestValidParentheses(self, s):
 		maxlen = 0
 		dp = [0] * len(s)
 		for i in xrange(1, len(dp)):
@@ -21,6 +43,7 @@ class Solution(object):
 			maxlen = max(dp[i], maxlen) 
 
 		return maxlen
+	"""
 
 def main():
 	str = "))(())(())((())))()((())()(()))())(((())))((())((((()()))()()((()())(()))))((((()()((())())())()))()))))(()))))()((())))())((()()))))(()))((((()(()))))(((((()("
