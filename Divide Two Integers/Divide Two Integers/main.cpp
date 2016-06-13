fdef1d8e3b6400e7d4d2a4221cbaf0a7450e1aad
qiannn <409339559@qq.com>
2016-06-13 21:10:26 +0800

    add Divide Two Integers

--- Divide Two Integers/Divide Two Integers/main.cpp
+++ Divide Two Integers/Divide Two Integers/main.cpp
@@ -8,9 +8,24 @@
 
 #include <iostream>
 
-int divide(int dividend, int divisor) {
-    return INT32_MAX;
-}
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        long long a = dividend >= 0 ? dividend : -(long long) dividend;
+        long long b = divisor >= 0 ? divisor : -(long long) divisor;
+        long long result = 0, shift = 31;
+        while (shift >= 0) {
+            if (a >= b << shift) {
+                a -= b << shift;
+                result += 1LL << shift;
+            }
+            shift--;
+        }
+        result = ((dividend ^ divisor) >> 31) ? (-result) : (result);
+        if (result > INT32_MAX) return INT32_MAX;
+        else return result;
+    }
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
