baec35a3cf6241f45e789ca91ff67a3210bbd642
apple <409339559@qq.com>
2016-09-11 11:29:21 +0800

    Median of Two Sorted Array

---
+++ Median of Two Sorted Arrays/Median of Two Sorted Arrays/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  Median of Two Sorted Arrays
+//
+//  Created by apple on 16/9/11.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        int halfLength = (m + n) / 2;
+        return 0.0;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    return 0;
+}
